19c08476c26aaf35341ebb1a8342ed2b8eb6d542
mahmood <mahmoodalmajed@gmail.com>
2025-02-17 11:50:15 +0300

    modfication

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -6,7 +6,7 @@ const WeatherForecast = (props) => {
             <div className="weather">
                 <h2>{props.day}</h2>
                 <img src={props.img} alt={props.imgAlt} />
-                <p><span>conditions: </span>{props.condition}</p>
+                <p><span>conditions: </span>{props.conditions}</p>
                 <p><span>time: </span>{props.time}</p>
             </div>
 
